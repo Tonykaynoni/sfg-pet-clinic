35428accad9a6886bfe69722ac3a277573c672b8
Tonykaynoni <tonykay001@gmail.com>
2019-08-05 15:12:38 +0100

    kikb kbk kbk

--- src/main/java/com/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/com/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -8,7 +8,7 @@ public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SfgPetClinicApplication.class, args);
-		System.out.print("ggjvdd");
+		System.out.print("ggjvd jugu d");
 	}
 
 }
